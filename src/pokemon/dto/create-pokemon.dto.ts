b4f8ceb7dd7ecbe907c897e8aa01d62a9e9ee4b6
rosousa <rosousa@pop-os.localdomain>
2023-07-12 02:26:50 -0300

    add pokemon entity and createdto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { IsInt, IsOptional, IsString } from 'class-validator';
+import { Pokemon } from '../entities/pokemon.entity';
+
+export class CreatePokemonDto implements Pokemon {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  @IsOptional()
+  height?: number | null;
+}
